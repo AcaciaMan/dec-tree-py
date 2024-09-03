6eede0c2070b1af9df3be9833b7dcc0bc325f94a
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-09-04 01:23:23 +0300

    Update m_shop_analysis.py

--- Python/src/m_shop/m_shop_analysis.py
+++ Python/src/m_shop/m_shop_analysis.py
@@ -3,13 +3,14 @@ from matplotlib import pyplot as plt
 from m_settings.m_settings import M_SettingsSingleton
 import pandas as pd
 import seaborn as sns
+import os
 
 class M_ShopAnalysis:
 
     def __init__(self):
         self.iset = M_SettingsSingleton()
         #load the dataframe from pickle
-        self.df = pd.DataFrame().from_pickle(self.iset.m_set['rootFolder'] +'/shop_data.pkl')
+        self.df = pd.read_pickle(self.iset.m_set['rootFolder'] +'/shop_data.pkl')
 
     #method to plot yearly sell per item name
     def plot_yearly_sell(self):
@@ -22,6 +23,13 @@ class M_ShopAnalysis:
         plt.figure(figsize=(10, 6))
         sns.lineplot(x='year', y='sell', hue='name', data=df_yearly_sell)
         plt.title('Yearly sell per item name')
+        #check if the folder exists, if not create it
+        if not os.path.exists(self.iset.getTryFolder()):
+            os.makedirs(self.iset.getTryFolder())
+
+
+
+
         plt.savefig(self.iset.getTryFolder() + '/yearly_sell.png')
 
     #method to plot yearly stock per item name
@@ -30,10 +38,10 @@ class M_ShopAnalysis:
         Plot the yearly stock per item name
         """
         #group by item name and year and sum the stock
-        df_yearly_stock = self.df.groupby(['name', 'year'])['stock'].sum().reset_index()
+        df_yearly_stock = self.df.groupby(['name', 'year'])['buy'].sum().reset_index()
         #plot the data
         plt.figure(figsize=(10, 6))
-        sns.lineplot(x='year', y='stock', hue='name', data=df_yearly_stock)
+        sns.lineplot(x='year', y='buy', hue='name', data=df_yearly_stock)
         plt.title('Yearly stock per item name')
         plt.savefig(self.iset.getTryFolder() + '/yearly_stock.png')
 
